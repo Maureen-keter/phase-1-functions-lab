9a9d42abef7abf46921849b21c03a8d98145c60c
maureen <maureen.chelangat@student.moringaschool.com>
2023-09-20 14:08:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupDistance){
+    return Math.abs(pickupDistance -42)}
+
+    function distanceFromHqInFeet(pickupDistance){
+        const blocks =distanceFromHqInBlocks(pickupDistance)
+        return blocks*264;
+    }
+   function distanceTravelledInFeet(start,end){
+    return Math.abs((start-end)*264);
+   }
+
+   function calculatesFarePrice(start, destination) {
+    const feetTraveled = distanceTravelledInFeet(start, destination);
+  
+    if (feetTraveled <= 400) {
+      return 0; 
+    } 
+    else if (feetTraveled <= 2000) {
+      return (feetTraveled - 400) * 0.02; 
+    }
+     else if (feetTraveled <= 2500) {
+      return 25; 
+    } 
+    else {
+      return 'cannot travel that far'; 
+    }
+  }
+  
+  
+  
+  
+  
